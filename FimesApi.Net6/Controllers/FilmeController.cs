f6bb6eacec081ee18ef9475b6703639c05234ebd
Rafael Menezes <rafa.rcm@hotmail.com>
2023-12-26 10:55:26 -0300

    Implementando Delete

--- FimesApi.Net6/Controllers/FilmeController.cs
+++ FimesApi.Net6/Controllers/FilmeController.cs
@@ -4,6 +4,7 @@ using FimesApi.Net6.Data.Dtos;
 using FimesApi.Net6.Models;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace FimesApi.Net6.Controllers;
 
@@ -31,9 +32,9 @@ public class FilmeController : ControllerBase
     }
 
     [HttpGet]
-    public IEnumerable<Filme> RecuperaFilme([FromQuery] int skip = 0, [FromQuery] int take = 50)
+    public IEnumerable<ReadFilmeDto> RecuperaFilme([FromQuery] int skip = 0, [FromQuery] int take = 50)
     {
-        return _context.Filmes.Skip(skip).Take(take);
+        return _mapper.Map<List<ReadFilmeDto>>(_context.Filmes.Skip(skip).Take(take));
     }
 
     [HttpGet("{id}")]
@@ -44,7 +45,8 @@ public class FilmeController : ControllerBase
         {
             return NotFound();
         }
-        return Ok(filme);
+        var filmeDto = _mapper.Map<ReadFilmeDto>(filme);
+        return Ok(filmeDto);
     }
 
     [HttpPut("{id}")]
@@ -52,7 +54,7 @@ public class FilmeController : ControllerBase
     {
         var filme = _context.Filmes.FirstOrDefault(
             filme => filme.Id == id);
-        if(filme == null)
+        if (filme == null)
         {
             return NotFound();
         }
@@ -81,6 +83,20 @@ public class FilmeController : ControllerBase
         _mapper.Map(filmeUpdate, filme);
         _context.SaveChanges();
 
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeletaFilme(int id)
+    {
+        var filme = _context.Filmes.FirstOrDefault(
+            filme => filme.Id == id);
+        if (filme == null)
+        {
+            return NotFound();
+        }
+        _context.Remove(filme);
+        _context.SaveChanges();
         return NoContent();
     }
 }
